bbffb5198844e3a410140a9f1391a45a369c49e0
reinanbr <perseu912@gmail.com>
2023-11-01 04:13:31 -0300

    success

--- src/tools/resize_for_text.py
+++ src/tools/resize_for_text.py
@@ -1,5 +1,5 @@
 base_px = 245
-char_px = 4.5
+char_px = 5.5
 
 def resize_for_text(text):
     size = len(text)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from src.create_template import create_painel_food as cpf
 
 
-cpf('feijão e artgrtgrtgrtfffrroz ','test.png')
+cpf('Bolacha com Vitamina e Fruta ','test.png')
