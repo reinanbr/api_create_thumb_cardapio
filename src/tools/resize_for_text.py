a5d83bd07a9fd3f1d56d0faff22c84e68ed65cca
reinanbr <perseu912@gmail.com>
2023-11-17 13:56:01 -0300

    added days new year

--- src/tools/resize_for_text.py
+++ src/tools/resize_for_text.py
@@ -1,12 +1,12 @@
 base_px = 700
-char_px = 11
+char_px = 15
 
 def resize_for_text(text):
     size = len(text)
-    if size > 6:
+    if size > 5:
         w_n = len(text.split())
         print(f'size: {size}')
-        x = base_px - char_px*(size-6) + w_n*16
+        x = base_px - char_px*(size-6) + w_n*20
         return x
     else:
         return base_px
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from src.create_template import create_painel_food as cpf
 
 
-print(cpf('Bolacha com Vitamina e Fruta ','test.png'))
+print(cpf('Bolacha e Suco','test.png'))
