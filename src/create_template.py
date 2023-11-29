aef4ac7b044efe0c8e9a1def5d166f0d63d37be1
reinanbr <perseu912@gmail.com>
2023-11-29 15:17:24 -0300

    fix temp climate

--- src/create_template.py
+++ src/create_template.py
@@ -47,7 +47,7 @@ def _create_painel_food_(food_name:str,path:str='test.png'):
     
     percent_cloud = (data_now['cloud']+data_now['rain_percent'])
     print(percent_cloud)
-    draw.text((280,1180),f"Petrolina-PE    {(data_now['temperature']+2.5):.1f}ºC","#000",font=font_climate)
+    draw.text((280,1180),f"Petrolina-PE    {(data_now['temperature']+1.1):.1f}ºC","#000",font=font_climate)
     draw.text((1180,1165),f'Humidade: {data_now["humidity"]:.1f}%','grey',font=font_climate_small)
     draw.text((1180,1225),f'Vento: {(data_now["v"]*3.6):.1f}km/h','grey',font=font_climate_small)
     draw.text((790,1180),get_emoji_cloud(percent_cloud),'#000',font=font_emoji)
