f77f11319ba58f3d44c91a969fa6aaf51964f44a
reinanbr <perseu912@gmail.com>
2023-12-04 15:08:43 -0300

    que porra é essa?

--- src/tools/get_usd.py
+++ src/tools/get_usd.py
@@ -5,6 +5,7 @@ def get_brl(minutes=1440):
     data_usd = req.get(f'https://api-forex-py.vercel.app/usd?minutes={minutes}').json()
 
     data_usd_brl = data_usd['dolar']['now_time']['BRL']
+    print(data_usd_brl)
     return data_usd_brl
 
 
